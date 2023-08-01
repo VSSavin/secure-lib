9f8e03e8149e4b8bbf91dbc0c597722cbdbca42e
vssavin <savin1801@gmail.com>
2023-08-01 15:44:44 +0300

    Utils class - some refactoring

--- src/main/java/io/github/vssavin/securelib/Utils.java
+++ src/main/java/io/github/vssavin/securelib/Utils.java
@@ -15,26 +15,31 @@ public final class Utils {
     public static void clearString(String string) {
         try {
             Field stringChars = String.class.getDeclaredField("value");
-            try {
-                stringChars.setAccessible(true);
-            } catch (Exception inaccessible) {
-                String errorMessage = "setAccessible on string chars may throw an InaccessibleObjectException\n" +
-                        "for some versions of JVM. To solve this problem add vm option:\n" +
-                        "--add-opens java.base/java.lang=ALL-UNNAMED";
-                System.err.println(errorMessage);
-                throw new RuntimeException(errorMessage, inaccessible);
-            }
-
-            try {
-                char[] chars = (char[]) stringChars.get(string);
-                Arrays.fill(chars, '*');
-            } catch (ClassCastException castException) {
-                byte[] bytes = (byte[]) stringChars.get(string);
-                Arrays.fill(bytes, (byte) '*');
-            }
-
+            setFieldAccessible(stringChars);
+            maskStringChars(stringChars, string);
         } catch (Exception e) {
-            throw new RuntimeException("String clearing error!", e);
+            throw new IllegalStateException("String clearing error!", e);
+        }
+    }
+
+    private static void setFieldAccessible(Field fieldAccessible) {
+        try {
+            fieldAccessible.setAccessible(true);
+        } catch (Exception inaccessible) {
+            String errorMessage = "setAccessible on string chars may throw an InaccessibleObjectException\n" +
+                    "for some versions of JVM. To solve this problem add vm option:\n" +
+                    "--add-opens java.base/java.lang=ALL-UNNAMED";
+            throw new IllegalStateException(errorMessage, inaccessible);
+        }
+    }
+
+    private static void maskStringChars(Field stringChars, String string) throws IllegalAccessException {
+        try {
+            char[] chars = (char[]) stringChars.get(string);
+            Arrays.fill(chars, '*');
+        } catch (ClassCastException castException) {
+            byte[] bytes = (byte[]) stringChars.get(string);
+            Arrays.fill(bytes, (byte) '*');
         }
     }
 }
